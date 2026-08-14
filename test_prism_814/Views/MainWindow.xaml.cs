7ac17f1579ba533d4a337f78e95e58840090c3fe
DNX <DNX@DESKTOP-Q5241BM>
2026-08-14 09:57:56 +0800

    添加项目文件。

---
+++ test_prism_814/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace test_prism_814.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
