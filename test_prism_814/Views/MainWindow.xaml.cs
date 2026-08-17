41966acdfbfc23eeedfdbeb8381c39ac1dfb8a1c
DNX <DNX@DESKTOP-Q5241BM>
2026-08-17 17:44:51 +0800

    8/17 222

--- test_prism_814/Views/MainWindow.xaml.cs
+++ test_prism_814/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Prism.Ioc;
+using Prism.Regions;
 using System.Windows;
 using test_prism_814.Models;
 using test_prism_814.ViewModels;
@@ -10,11 +11,13 @@ namespace test_prism_814.Views
         public MainWindow(User currentUser)
         {
             InitializeComponent();
+            var region = Prism.Ioc.ContainerLocator.Current.Resolve<Prism.Regions.IRegionManager>();
             // 传入当前用户信息给 ViewModel
             this.DataContext = new MainWindowViewModel(
-                Prism.Ioc.ContainerLocator.Current.Resolve<Prism.Regions.IRegionManager>(),
-                currentUser
+               region,
+               currentUser
             );
+            RegionManager.SetRegionManager(A, region);
         }
     }
 }
